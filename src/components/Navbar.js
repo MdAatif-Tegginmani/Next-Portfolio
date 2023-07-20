257bb91bfdbc43ac27ea2ddcc8cfb31e7a608d4e
Md Aatif Tegginmani <mdaatif84@gmail.com>
2023-07-20 23:59:54 +0530

    new m

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
-import Logo from './Logo'
 import { useRouter } from 'next/router'
-import { GithubIcon, LinkedInIcon, TwitterIcon } from './Icons'
+import { GithubIcon, LinkedInIcon, MoonIcon, SunIcon, TwitterIcon } from './Icons'
 import { motion } from 'framer-motion'
+import useThemeSwitcher from './hooks/useThemeSwitcher';
 
 const CustomLink = ({ href, title, className = "" }) => {
     const router = useRouter();
@@ -16,6 +16,10 @@ const CustomLink = ({ href, title, className = "" }) => {
 }
 
 const Navbar = () => {
+
+    const [mode, setMode] = useThemeSwitcher();
+
+
     return (
         <header className='w-full px-32 py-8 font-medium flex items-center justify-between '>
 
@@ -26,9 +30,12 @@ const Navbar = () => {
 
 
             </nav>
-            <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+            {/* <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
                 <Logo />
             </div>
+    */}
+
+
             <nav className='flex items-center justify-center flex-wrap'>
                 <motion.a href='https://twitter.com' target={"_blank"}
                     whileHover={{ y: -3 }}
@@ -55,6 +62,19 @@ const Navbar = () => {
                     <Link href='/' target={"_blank"}>T</Link>
 
                 */}
+
+                <button
+                    onClick={() => setMode(mode === "light" ? "dark" : "light")}
+                    className='ml-3 flex items-center justify-center rounded-full p-1'
+                >
+                    {
+                        mode === "dark" ? (
+                            <SunIcon className={"fill-dark"} />
+                        ) : (
+                            <MoonIcon className={"fill-dark"} />
+                        )
+                    }
+                </button>
             </nav>
 
         </header>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
     </Head>
-    <main className='bg-light w-full min-h-screen '>
+    <main className='bg-light dark:bg-dark w-full min-h-screen '>
     <Navbar/>
     <Component {...pageProps} />
     <Footer />
